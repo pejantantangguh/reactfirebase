519c5109408ba4c02e99792abed4809a5d1a490f
pejantantangguh <suherman.ng@gmail.com>
2019-04-15 21:35:01 +1000

    Sign In to Google, Signout and Sign up partly working

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {firestore} from '../firebase';
+import {firestore, auth} from '../firebase';
 
 class AddPost extends Component {
   state = { title: '', content: '' };
@@ -13,15 +13,16 @@ class AddPost extends Component {
     event.preventDefault();
 
     const { title, content } = this.state;
+    const {uid, displayName, email, photoURL} = auth.currentUser || {};
 
     const post = {
       title,
       content,
       user: {
-        uid: '',
-        displayName: '',
-        email: '',
-        photoURL: '',
+        uid,
+        displayName,
+        email,
+        photoURL
       },
       favorites: 0,
       comments: 0,
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,44 +1,59 @@
 import React, {Component} from 'react';
+import {auth} from '../firebase';
 
 class SignUp extends Component {
     state = {displayName:'', email:'', password:''};
 
-    handleSubmit = e => {
-        e.preventDefault();
-        this.setState({displayName:'',email:'',password:''})
-    }
-
     handleChange = e => {
         const {name,value} = e.target;
-        this.setState({[name]: value});
-    }
+        this.setState({[name]:value})
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        
+        const {email,password, displayName} = this.state;
+
+        try {
+            const {user} = await auth.createUserWithEmailAndPassword(
+                email,
+                password
+            );
+            user.updateProfile({displayName});
+        } catch (error) {
+            console.error(error);
+        }
+        this.setState({displayName:'',email:'',password:''}) // Set state to empty
+    };
+
+
 
     render(){
-        const {displayName, email,password} = this.state;
+        const {name,email,password} = this.state;
         return(
             <form className="SignUp" onSubmit ={this.handleSubmit}>
                 <h2>Sign Up</h2>
                 <input 
                     type="text"
-                    name={displayName}
+                    name="name"
                     placeholder="Display Name"
-                    value={displayName}
-                    onChange = {this.handleChange}
+                    value={name}
+                    onChange={this.handleChange}
                     />
                 <input
                     type="email"
-                    name={email}
+                    name="email"
                     placeholder="sample@example.com"
                     value={email}
-                    onChange = {this.handleChange}
+                    onChange={this.handleChange}
                     />
 
                 <input 
                     type="password"
-                    name={password}
+                    name="password"
                     placeholder="Password"
                     value={password}
-                    onChange = {this.handleChange}
+                    onChange={this.handleChange}
                 />
                 
                 <input type="submit" value="Sign Up" /> 
